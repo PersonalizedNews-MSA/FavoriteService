b1f974f6b978ba32ef6409471e2dd8cfa9453926
seoyoun8694 <chaeseoyoun8694@gmail.com>
2025-06-23 21:04:28 +0900

    fix. kafka timestamp localdatetime 수정

--- src/main/java/com/mini2/favorite_service/kafka/dto/FavoriteEventDto.java
+++ src/main/java/com/mini2/favorite_service/kafka/dto/FavoriteEventDto.java
@@ -1,5 +1,6 @@
 package com.mini2.favorite_service.kafka.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -11,6 +12,7 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 public class FavoriteEventDto {
     private String eventId;
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss.SSS")
     private LocalDateTime timestamp;
     private String sourceService;
     private FavoritePayloadDto payload;
